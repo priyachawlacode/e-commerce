32ea686cd0895f2ef5630264e83c294c004db5d6
Priya Chawla <priyachawla230@gmail.com>
2020-04-17 18:47:37 +0530

    Add files via upload

---
+++ New folder/practice/practice/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+"""def login(request):
+    return render(request,"login.html")
+def hello(request):
+    nam=request.GET.get('name','default')
+    param={'naam':nam}
+    return render(request,'home.html',param)"""
