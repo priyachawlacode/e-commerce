ff13ec4a5ebb3d86f1831e864da99fd1e1794598
Priya Chawla <priyachawla230@gmail.com>
2020-04-17 18:51:41 +0530

    Add files via upload

---
+++ New folder/practice/shop/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product,Customer
+
+admin.site.register(Product)
+
+#admin.site.register(Employee)
+
+admin.site.register(Customer)
